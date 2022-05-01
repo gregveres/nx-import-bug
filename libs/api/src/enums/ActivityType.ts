c8be2cd0d9eadc468a1b9c200543cb34e178dc15
Greg Veres <greg@veresfamily.me>
2022-05-01 14:05:37 -0400

    cleared everything out to reframe the bug
    The previous issue probably wasn't a bug, but a misunderstanding of how to use a buildable library.
    I have deleted that case to get ready for another bug setup of a very similar setup.

--- libs/api/src/enums/ActivityType.ts
+++
@@ -1,8 +0,0 @@
-export enum ActivityType {
-  SinglesLeague = 0,
-  DoublesLeague = 1,
-  SinglesScheduledLeague = 2,
-  DoublesScheduledLeague = 3,
-  TeamLeague = 4,
-  TeamScheduledLeague = 5
-}
--- libs/helpers/src/lib/helpers.ts
+++
@@ -1,6 +0,0 @@
-import { ActivityType } from '@myorg/api/enums/ActivityType'
-
-export function helpers(): string {
-  console.log(ActivityType.DoublesLeague)
-  return 'helpers';
-}
